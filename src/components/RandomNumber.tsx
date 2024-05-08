183603fb0248d4221b83d2dc877e0b8e13a68d04
Suba177 <mikhail.sard@mail.ru>
2024-05-09 03:56:19 +0700

    working properly random

--- src/components/RandomNumber.tsx
+++ src/components/RandomNumber.tsx
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
 
 export default function RandomNumber() {
-  const [numbers, setNumbers] = useState(0);
+  const [numbers, setNumbers] = useState(undefined);
   const [timerId, setTimerId] = useState();
 
-  const add = () => setNumbers(Math.ceil(Math.random() * 10));
-  const start = () => setTimerId(setInterval(add, 500));
-  const stop = () => (clearInterval(timerId), setTimerId(null));
-  const clear = () => setNumbers();
+  const add = (min, max) => {
+    setNumbers(Math.floor(min + Math.random() * (max + 1 - min)));
+  };
+  // const start = () => setTimerId(setInterval(add, 500));
+  // const stop = () => (clearInterval(timerId), setTimerId(null));
+  const clear = () => setNumbers(null);
 
   console.log(numbers);
   return (
     <div>
       <div>
-        <button onClick={start} disabled={timerId}>
+        {/* <button onClick={start} disabled={timerId}>
           Старт
         </button>
         <button onClick={stop} disabled={!timerId}>
           Стоп
-        </button>
+        </button> */}
         <button onClick={clear}>Очистить</button>
       </div>
       <ul>
-        <button onClick={add} id="spin">
+        <button onClick={() => add(1, 10)} id="spin">
           Spin!
         </button>
         <span className="arrow"></span>
