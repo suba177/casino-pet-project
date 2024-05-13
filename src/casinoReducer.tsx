d46943ff04e9f7a5f7ae93ebf1cc434036d29522
Suba177 <mikhail.sard@mail.ru>
2024-05-13 09:07:53 +0700

    working code

--- src/casinoReducer.tsx
+++ src/casinoReducer.tsx
@@ -1,9 +1,11 @@
-const defaultState = { value: null };
+const defaultState = { value: null, betValue: 0 };
 
 export const casinoReducer = (state = defaultState, action) => {
   switch (action.type) {
     case "GET_INTEGER":
       return { ...state, value: action.payload };
+    case "GET_BETNUMBER":
+      return { ...state, betValue: action.payload };
     default:
       return state;
   }
--- src/components/Wheel.tsx
+++ src/components/Wheel.tsx
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
+import NavigationIcon from "@mui/icons-material/Navigation";
 
 export default function Wheel() {
   const dispatch = useDispatch();
   const value = useSelector((state) => state.casinoReducer.value);
+  const betValue = useSelector((state) => state.casinoReducer.betValue);
   const betButton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   const getInteger = () => {
@@ -15,6 +17,10 @@ export default function Wheel() {
       return Math.floor(rand);
     };
     dispatch({ type: "GET_INTEGER", payload: randomInteger() });
+  };
+
+  const getBetNum = (item) => {
+    dispatch({ type: "GET_BETNUMBER", payload: item });
   };
 
   return (
@@ -31,13 +37,46 @@ export default function Wheel() {
         Крутить!
       </Button>
       <Typography sx={{ color: "white", fontSize: "20px" }}>
-        Поставить на:
+        Поставить на: {betValue}
       </Typography>
       {betButton.map((item, index) => (
-        <Button color="inherit" size="large" variant="contained" key={index}>
+        <Button
+          onClick={() => getBetNum(item)}
+          color="inherit"
+          size="large"
+          variant="contained"
+          key={index}
+        >
           {item}
         </Button>
       ))}
+      {value === betValue ? (
+        <div style={{ color: "white", fontSize: "20px" }}>MATCH!</div>
+      ) : null}
+      <div className="roulette">
+        <NavigationIcon
+          sx={{
+            position: "absolute",
+            left: "50%",
+            transform: "translate(-50%, 160px)",
+          }}
+          color="info"
+        />
+        <div className="scopeHidden">
+          <ul style={{ color: "white", fontSize: "20px" }}>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>10</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
