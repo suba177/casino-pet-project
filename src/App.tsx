a1fb0765b246c663e4a2f64f09f828a332d5ca76
Suba177 <mikhail.sard@mail.ru>
2024-05-13 13:05:34 +0700

    working code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./style/index.css";
 import "./style/reset.css";
-import Wheel from "./components/Wheel";
+import Roulette from "./components/Roulette";
 import Layout from "./components/Layout";
 import About from "./components/About";
 import Rules from "./components/Rules";
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Wheel />} />
+        <Route index element={<Roulette />} />
         <Route path="about" element={<About />} />
         <Route path="rules" element={<Rules />} />
       </Route>
--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -2,12 +2,14 @@ import { useDispatch, useSelector } from "react-redux";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
 import NavigationIcon from "@mui/icons-material/Navigation";
+import { useRef, useEffect } from "react";
 
-export default function Wheel() {
+export default function Roulette() {
   const dispatch = useDispatch();
   const value = useSelector((state) => state.casinoReducer.value);
   const betValue = useSelector((state) => state.casinoReducer.betValue);
   const betButton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const listRef = useRef(null);
 
   const getInteger = () => {
     const randomInteger = () => {
@@ -16,7 +18,11 @@ export default function Wheel() {
       const rand = min + Math.random() * (max + 1 - min);
       return Math.floor(rand);
     };
+
     dispatch({ type: "GET_INTEGER", payload: randomInteger() });
+    const elem = document.querySelector("ul");
+    const move = -1500;
+    elem.style.left = move + "px";
   };
 
   const getBetNum = (item) => {
@@ -28,14 +34,7 @@ export default function Wheel() {
       <Typography sx={{ color: "white", fontSize: "20px" }}>
         Cлучайное число: {value}
       </Typography>
-      <Button
-        onClick={() => getInteger()}
-        color="inherit"
-        size="large"
-        variant="contained"
-      >
-        Крутить!
-      </Button>
+
       <Typography sx={{ color: "white", fontSize: "20px" }}>
         Поставить на: {betValue}
       </Typography>
@@ -62,21 +61,29 @@ export default function Wheel() {
           }}
           color="info"
         />
-        <div className="scopeHidden">
-          <ul style={{ color: "white", fontSize: "20px" }}>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>10</li>
-          </ul>
-        </div>
       </div>
+      <div className="numbers">
+        <ul style={{ color: "white", fontSize: "20px" }}>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>10</li>
+        </ul>
+      </div>
+      <Button
+        onClick={() => getInteger()}
+        color="inherit"
+        size="large"
+        variant="contained"
+      >
+        Крутить!
+      </Button>
     </>
   );
 }
